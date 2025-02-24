cd8cd13741cafe835b415c8bbf3b947dc21715e8
Patrick <ptobou@yahoo.fr>
2025-02-24 17:11:17 +0100

    Added user profile feed API

--- core/views.py
+++ core/views.py
@@ -6,6 +6,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
 from core import serializers
 from core import models, permissions
@@ -104,8 +105,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
-    authentification_classes = (TokenAuthentication,)
-    permission_classes = (permissions.UpdateOwnProfile,)
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,IsAuthenticated,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
 
@@ -113,3 +114,22 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 class UserLoginApiView(ObtainAuthToken):
     """Handle creating user authentication tokens"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (
+        IsAuthenticated,
+        permissions.UpdateOwnStatus,
+        )
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        print(f"Utilisateur actuel : {self.request.user}, Authentifié : {self.request.user.is_authenticated}")
+
+        if not self.request.user.is_authenticated:
+            raise serializers.ValidationError("User must be authenticated to post a feed item.")
+        serializer.save(user_profile=self.request.user)
